474d1db65243eb01931ec4e1e492761a5b606e7a
Sanchit Mittal <sanchitmittal18@gmail.com>
2017-10-05 14:14:53 +0530

    ontouch click sector of circle

---
+++ Animation 1/DrawView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawView.swift
+//  Animation 1
+//
+//  Created by Sanchit Mittal on 04/10/17.
+//  Copyright © 2017 Sanchit Mittal. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
