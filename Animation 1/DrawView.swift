5fff08ac4781ba46310064d5a0d4b42cb472c8ba
Sanchit Mittal <sanchitmittal18@gmail.com>
2017-10-05 16:41:03 +0530

    code added

--- Animation 1/DrawView.swift
+++ Animation 1/DrawView.swift
@@ -8,14 +8,72 @@
 
 import UIKit
 
+@IBDesignable
 class DrawView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var colorPath: UIBezierPath? {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-    */
+    
+    var paths: [UIBezierPath]?
+    var centre: CGPoint = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+    
+    @IBInspectable
+    var radius: CGFloat = 100 { didSet { setNeedsDisplay() } }
+    
+    @IBInspectable
+    var numberOfSections: Int = 6 { didSet { setNeedsDisplay() } }
 
+    @IBInspectable
+    var lineWidth: CGFloat = 5 { didSet { setNeedsDisplay() } }
+    
+    override func draw(_ rect: CGRect) {
+        paths?.removeAll()
+        var startAngle: CGFloat = 0
+        while startAngle < CGFloat(2*Double.pi) {
+            let path = definePath(startAngle: &startAngle)
+            paths?.append(path)
+            path.stroke()
+            colorCircleIn(path: path)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        checkTouchInCircle(touch: touch!)
+    }
+    
+    
+    func definePath(startAngle: inout CGFloat) -> UIBezierPath {
+        let path = UIBezierPath()
+        path.lineWidth = lineWidth
+        path.addArc(withCenter: centre, radius: radius, startAngle: startAngle, endAngle: startAngle + (CGFloat(2*Double.pi) / CGFloat(numberOfSections)), clockwise: true)
+        startAngle = startAngle + CGFloat(2*Double.pi) / CGFloat(numberOfSections)
+        path.addLine(to: centre)
+        path.close()
+        return path
+    }
+    
+    func colorCircleIn(path: UIBezierPath) {
+        if path == colorPath {
+            UIColor.red.setFill()
+            path.fill()
+        }
+    }
+    
+    func checkTouchInCircle(touch: UITouch) {
+        var startAngle: CGFloat = 0
+        while startAngle < CGFloat(2*Double.pi) {
+            let path = definePath(startAngle: &startAngle)
+            if path.contains((touch.location(in: self))) {
+                colorPath = path
+                setNeedsDisplay()
+                return
+            }
+        }
+    }
+//    func startAnimation() {
+//        colorPath = paths?[2]
+//    }
 }
--- Animation 1/ViewController.swift
+++ Animation 1/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var drawView: DrawView!
+    
+    @IBAction func startAnimation(_ sender: UIButton) {
+////        UIView.animate(withDuration: 4, animations: { () -> Void in
+//            UIView.animate(withDuration: 10, delay: 0.4, options: [],
+//                           animations: {
+//                            self.drawView.transform = .concatenating(<#T##CGAffineTransform#>)
+//            }            )//
+////        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func stopAnimation(_ sender: UIButton) {
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
-
